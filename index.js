d22b33001fe1e90c60127f51fcbed333f74897c2
m <michal@duszyk.com>
2013-08-26 17:43:08 +0200

    cleaned index.js

--- index.js
+++ index.js
@@ -8,9 +8,5 @@ var LOG = new logging.Logger(path.basename(module.filename));
 
 LOG.info("Initializing Scalextreme APM plugin...");
 
-LOG.debug("Debug log: %s-%s-%s", 1, 2, 3);
-LOG.info("Info log: %s", 2);
-LOG.warn("Warn log: %s", 3);
-LOG.error("Error log: %s", 4);
 
 wrapping.wrap(require('module'), 'module', '_load', wrapper_module._loadWrapperFactory);
