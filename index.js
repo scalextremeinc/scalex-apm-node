c8c84487cf96d419e9e280c5abe022dff1a5c796
m <michal@duszyk.com>
2013-08-28 17:15:26 +0200

    initial express support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var path = require('path')
 var logging = require(path.join(__dirname, 'lib', 'logging'));
 var obj_patch = require(path.join(__dirname, 'lib', 'obj_patch'));
+var wrapper = require(path.join(__dirname, 'lib', 'wrapper', 'wrapper'));
 var wrapper_module = require(path.join(__dirname, 'lib', 'wrapper', 'module', 'module'));
 var wrapper_http = require(path.join(__dirname, 'lib', 'wrapper', 'module', 'http'));
 
@@ -11,4 +12,6 @@ LOG.info("Initializing Scalextreme APM plugin...");
 
 obj_patch.add(require('http'), 'http', 'request', wrapper_http.request);
 obj_patch.add(require('http'), 'http', 'createServer', wrapper_http.createServer);
+
+
 obj_patch.add(require('module'), 'module', '_load', wrapper_module._load);
