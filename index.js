c0db2b7c5254e0723996da2c5b0fed42d3b09b7e
m <michal@duszyk.com>
2013-08-29 14:03:57 +0200

    added express wrapper

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ var lib_dir = path.join(__dirname, 'lib');
 
 var logging = require(path.join(lib_dir, 'logging'));
 var obj_patch = require(path.join(lib_dir, 'obj_patch'));
-var wrapper = require(path.join(lib_dir, 'wrapper'));
 var wrapper_module = require(path.join(lib_dir, 'wrapper', 'module'));
-var wrapper_http = require(path.join(lib_dir, 'wrapper', 'http'));
 
-var LOG = new logging.Logger(path.basename(module.filename));
 //logging.CONFIG.level = logging.INFO;
+var LOG = new logging.Logger(path.basename(module.filename));
 
 LOG.info("Initializing Scalextreme APM plugin...");
 
-obj_patch.add(require('http'), 'http', 'request', wrapper_http.request);
-obj_patch.add(require('http'), 'http', 'createServer', wrapper_http.createServer);
-
-
-obj_patch.add(require('module'), 'module', '_load', wrapper_module._load);
+var module = require('module');
+obj_patch.add(module, 'module', '_load', wrapper_module._load);
+module._scalex_patched = true;
