ee20b1c7d41d0039fe4f14e11c34a396f8e30265
m <michal@duszyk.com>
2013-08-29 13:34:08 +0200

    reorganized dir structure

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var path = require('path')
-var logging = require(path.join(__dirname, 'lib', 'logging'));
-var obj_patch = require(path.join(__dirname, 'lib', 'obj_patch'));
-var wrapper = require(path.join(__dirname, 'lib', 'wrapper', 'wrapper'));
-var wrapper_module = require(path.join(__dirname, 'lib', 'wrapper', 'module', 'module'));
-var wrapper_http = require(path.join(__dirname, 'lib', 'wrapper', 'module', 'http'));
+var path = require('path');
+var lib_dir = path.join(__dirname, 'lib');
+
+var logging = require(path.join(lib_dir, 'logging'));
+var obj_patch = require(path.join(lib_dir, 'obj_patch'));
+var wrapper = require(path.join(lib_dir, 'wrapper'));
+var wrapper_module = require(path.join(lib_dir, 'wrapper', 'module'));
+var wrapper_http = require(path.join(lib_dir, 'wrapper', 'http'));
 
 var LOG = new logging.Logger(path.basename(module.filename));
 //logging.CONFIG.level = logging.INFO;
