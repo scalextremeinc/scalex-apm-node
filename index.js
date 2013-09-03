d46e2d21fee51e85d4cc68b09ecca636f37bd993
m <michal@duszyk.com>
2013-09-03 16:20:46 +0200

    added uploader

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ var path = require('path');
 var lib_dir = path.join(__dirname, 'lib');
 
 var logging = require(path.join(lib_dir, 'logging'));
-var obj_patch = require(path.join(lib_dir, 'obj_patch'));
-var wrapper_module = require(path.join(lib_dir, 'wrapper', 'module'));
-
 //logging.CONFIG.level = logging.INFO;
 var LOG = new logging.Logger(path.basename(module.filename));
 
 LOG.info("Initializing Scalextreme APM plugin...");
 
+// IoC
+var uploader = require(path.join(lib_dir, 'uploader'));
+var collector = require(path.join(lib_dir, 'collector'));
+var c = new collector.Collector();
+var u = new uploader.Uploader(c);
+u.start();
+
+// initialize module patching
+var obj_patch = require(path.join(lib_dir, 'obj_patch'));
+var wrapper = require(path.join(lib_dir, 'wrapper', 'module'));
 var module = require('module');
-obj_patch.add(module, 'module', '_load', wrapper_module._load);
+obj_patch.add(module, 'module', '_load', wrapper._load);
 module._scalex_patched = true;
